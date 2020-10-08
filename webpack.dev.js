2e4a5716626e41907cca3841401203607180bd05
suleiman <sl.mustapha98@gmail.com>
2020-10-08 08:16:38 +0100

    unmerge files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,42 @@
 const path = require("path");
-const common = require("./webpack.common");
-const { merge } = require("webpack-merge");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common, {
+module.exports = {
+  entry: "./src/client/index.js",
   mode: "development",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var",
+    library: "Client",
   },
   devtool: "source-map",
   stats: "verbose",
   module: {
     rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "name.[hash].[ext]", outputPath: "imgs" },
+        },
+      },
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
     ],
   },
-});
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,41 @@
 const path = require("path");
-const common = require("./webpack.common");
-const { merge } = require("webpack-merge");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 
-module.exports = merge(common, {
+module.exports = {
+  entry: "./src/client/index.js",
   mode: "production",
   output: {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var",
+    library: "Client",
   },
   optimization: {
     minimizer: [new OptimizeCssAssetsWebpackPlugin(), new TerserPlugin()],
   },
   module: {
     rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "name.[hash].[ext]", outputPath: "imgs" },
+        },
+      },
       {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
@@ -28,5 +46,9 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
     new CleanWebpackPlugin(),
     new WorkboxPlugin.GenerateSW(),
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
   ],
-});
+};
