ff624970b9bf8c952f1fd08cec72728cd8199b10
suleiman <sl.mustapha98@gmail.com>
2020-10-09 07:40:15 +0100

    correct my expect fn

--- __test__/score.test.js
+++ __test__/score.test.js
@@ -3,18 +3,18 @@ import { score } from "../src/client/js/formHandler";
 describe("test score polarity", () => {
   test("should return true", () => {
     const score_tag = "P+" || "P";
-    expect(score(score_tag).toBe("Positive"));
+    expect(score(score_tag)).toBe("Positive");
   });
   test("should return true", () => {
     const score_tag = "NEU";
-    expect(score(score_tag).toBe("Neutral"));
+    expect(score(score_tag)).toBe("Neutral");
   });
   test("should return true", () => {
     const score_tag = "N+" || "N";
-    expect(score(score_tag).toBe("Negative"));
+    expect(score(score_tag)).toBe("Negative");
   });
   test("should return true", () => {
     const score_tag = "NONE";
-    expect(score(score_tag).toBe("Non Sentimental"));
+    expect(score(score_tag)).toBe("Non Sentimental");
   });
 });
--- __test__/urlChecker.test.js
+++ __test__/urlChecker.test.js
@@ -3,25 +3,25 @@ import { checkForUrl } from "../src/client/js/urlChecker";
 describe("with url protocol", () => {
   test("should return true", () => {
     const input = "https://www.google.com";
-    expect(checkForUrl(input).toBe(true));
+    expect(checkForUrl(input)).toBe(true);
   });
   test("should return true", () => {
     const input = "http://www.google.com";
-    expect(checkForUrl(input).toBe(true));
+    expect(checkForUrl(input)).toBe(true);
   });
 });
 
 describe("without protocol", () => {
   test("should return true", () => {
     const input = "www.google.com";
-    expect(checkForUrl(input).toBe(true));
+    expect(checkForUrl(input)).toBe(true);
   });
   test("should return true", () => {
     const input = "google.com";
-    expect(checkForUrl(input).toBe(true));
+    expect(checkForUrl(input)).toBe(true);
   });
   test("should return false", () => {
     const input = "google . com";
-    expect(checkForUrl(input).toBe(false));
+    expect(checkForUrl(input)).toBe(false);
   });
 });
